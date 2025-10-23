ecd9d9db06b4c9c61cb31a135d68ff24a1905d0a
jihoon0306 <burning0306@gmail.com>
2025-10-23 14:25:48 +0900

    Update check.js

--- api/check.js
+++ api/check.js
@@ -1,7 +1,6 @@
 export default function handler(req, res) {
   res.status(200).json({
-    hasKey: !!process.env.TA_SERVICE_KEY,
-    // 값은 절대 노출하지 않음
+    hasKey: !!process.env.TA_SERVICE_KEY, // true면 키가 서버에 정상 주입
     now: new Date().toISOString()
   });
 }
